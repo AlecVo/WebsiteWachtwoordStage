533a93a4c327312199975a0a8c7041642a73a6d9
Alec Van Oosterwijck <alec@comm-it.local>
2022-03-23 16:10:44 +0100

    xommit

---
+++ CMS-SecureSend/SecureSend/App_Plugins/Iconic-233/iconic.prevalues.controller.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+angular.module("umbraco").controller("Koben.Iconic.Prevalues.Packages", ["$scope", "$http", "editorService", function ($scope, $http, editorService) {
+    $scope.overrideBgTemplate = false;
+
+    if (!angular.isArray($scope.model.value)) $scope.model.value = [];
+
+    $scope.createNewPackage = function () {
+        var newItem = new Package();
+
+        editorService.open({
+            title: "Create new package",
+            view: "/app_plugins/iconic/views/iconic.edit.dialog.html",
+            saved: function saved() {
+                $scope.model.value.push(angular.copy(newItem));
+            },
+            "package": newItem
+        });
+    };
+
+    $scope.editPackage = function (pkg) {
+        editorService.open({
+            title: "Edit package",
+            view: "/app_plugins/iconic/views/iconic.edit.dialog.html",
+            "package": pkg
+        });
+    };
+
+    $scope.removeItem = function (index) {
+        $scope.model.value.splice(index, 1);
+    };
+
+    $scope.toggleItemDisplay = function (item) {
+        if ($scope.selectedItem === item) {
+            $scope.selectedItem = null;
+        } else {
+            $scope.selectedItem = item;
+        }
+    };
+}]);
+
